2fd20fa340b603cf01b26f94d32cc6ff47344b1a
mahmoudhamed169 <mh4190207@gmail.com>
2022-12-12 00:13:44 +0200

    version 1.0.1

--- modules/department/controller/departmentController.js
+++ modules/department/controller/departmentController.js
@@ -3,11 +3,12 @@ const { StatusCodes } = require("http-status-codes");
 
 const Department = require("../model/departmentModel");
 const { StatusCodes } = require("http-status-codes");
+const User = require("../../users/model/userModel");
 
 const getAllDepartment = async (req, res) => {
   try {
     if (theUser.role == "admin") {
-      const departments = await Department.findALL({});
+      const departments = await Department.findALL({}).populate("The_Head");
       res.status(StatusCodes.ACCEPTED).json({ status:true ,message: "All Department", data: departments });
     } else {
       res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" });
@@ -21,18 +22,20 @@ const getAllDepartment = async (req, res) => {
 };
 
 const addNewDepartment = async (req, res) => {
-  let { departmentName } = req.body;
+  let { departmentName  , email } = req.body;
   if (theUser.role == "admin") {
     try {
-      const department = await Department.findOne({ departmentName });
-      if (department) {
-        res.status(StatusCodes.ACCEPTED).json({
-          massege: "department is already existes",
+      const user = await User.findOne({email})
+      const department = await Department.findOne({ departmentName : departmentName.toLowerCase() });
+      if (department ) {
+        res.status(StatusCodes.ACCEPTED).json({status :false,
+          message: "department is already existes",
         });
       } else {
-        let newDepartment = new Department({ departmentName });
+        if(!user){res.status(StatusCodes.CREATED).json({ status :false ,message: "invalid email" });}
+        let newDepartment = new Department({ departmentName : departmentName.toLowerCase() , The_Head : user._id });
         await newDepartment.save();
-        res.status(StatusCodes.CREATED).jsonjson({ massege: "Added success", Department: newDepartment });
+        res.status(StatusCodes.CREATED).json({status :false , message: "Added success", Department: newDepartment });
       }
     } catch (error) {
       res
@@ -48,10 +51,16 @@ const addNewDepartment = async (req, res) => {
 const updateDepartment = async (req, res) => {
   try {
     if (theUser.role == "admin") {
-      let { _id } = req.params;
-      let { The_Head } = red.body;
-      const department = await Department.updateOne({_id}, { The_Head });
-      res.status(StatusCodes.OK).json({ message: "Update done" });
+      
+      let { departmentName ,email } = red.body;
+      const user = await User.findOne({email})
+      if(!user){res.status(StatusCodes.CREATED).json({ status :false ,message: "invalid email" });}
+      const department = await Department.findOneAndUpdate({departmentName : departmentName.toLowerCase} , {The_Head:user._id})
+      
+
+      
+      
+      res.status(StatusCodes.OK).json({status:true, message: "Update done" , department });
     } else {
       res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" });
     }
@@ -68,9 +77,9 @@ const updateDepartment = async (req, res) => {
 const deleteDepartment = async (req, res) => {
   try {
     if (theUser.role == "admin") {
-      let { _id } = req.params;
+      let { departmentName } = req.body;
       
-      const department = await Department.deleteOne({ _id: id })
+      const department = await Department.findByIdAndDelete({departmentName :departmentName.toLowerCase()})
       res.status(StatusCodes.OK).json({ message: "deleted success" });
     } else {
       res.status(StatusCodes.UNAUTHORIZED).json({ message: "UNAUTHORIZED" });
