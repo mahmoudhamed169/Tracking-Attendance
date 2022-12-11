7516963e57f584113fb09747438d4a00a75e56d4
mahmoudhamed169 <mh4190207@gmail.com>
2022-12-12 00:17:13 +0200

    version 1.0.2

--- modules/department/routes/departmentRoutes.js
+++ modules/department/routes/departmentRoutes.js
@@ -1,9 +1,16 @@
 const validtateReq = require('../../../common/validtateReq');
 const isAuthenticated = require("../../../common/isAuthrized");
+const { getAllDepartment, addNewDepartment, updateDepartment, deleteDepartment } = require('../controller/departmentController');
 
 
 
 const router = require("express").Router();
+
+
+router.get("/Departments" , isAuthenticated(),getAllDepartment);
+router.post('/addNewDepartment' ,isAuthenticated(), addNewDepartment);
+router.put('/updateDepartment' ,isAuthenticated(), updateDepartment);
+router.delete('/deleteDepartment' , isAuthenticated(),deleteDepartment)
 
 
 
