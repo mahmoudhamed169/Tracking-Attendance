688798e4af5ed9502c29deedd4746ef549fc88ea
mahmoudhamed169 <mh4190207@gmail.com>
2022-12-12 21:28:21 +0200

    version 1.0.6

--- modules/users/routes/userRoutes.js
+++ modules/users/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const isAuthrized = require("../../../common/isAuthrized");
 const validateRequest = require("../../../common/validtateReq");
-const { getAllUsers, verifyUser, UnverifyUser, sign_up, sign_in, setUserRole, setUserDepartment } = require("../controller/userController");
+const { getAllUsers, verifyUser, UnverifyUser, sign_up, sign_in, setUserRole, setUserDepartment, getAllEmplyee, getAllHead } = require("../controller/userController");
 const router = require("express").Router();
 
 
@@ -14,6 +14,8 @@ router.get("/unverifyUser/:token", UnverifyUser);
 router.post('/signIn' , sign_in)
 router.post('/serRole' , isAuthrized() , setUserRole )
 router.post('/setDepartment' , isAuthrized() , setUserDepartment )
+router.get('/employee' , isAuthrized() , getAllEmplyee );
+router.get('/heads' , isAuthrized() , getAllHead)
 
 
 
